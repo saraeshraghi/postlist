af6db0df7ff728e6152dfaf781850c84da27a0e6
Sarajoooon <saraeshraghi.sa@gmail.com>
2022-04-26 22:49:37 +0430

    t1

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,5 @@ from rest_framework.authtoken import views as auth_token
 app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('ranking/<int:pk>', views.RankingView.as_view(), name='ranking')
-    # path('api_token/', auth_token.obtain_auth_token)
+    path('ranking/<int:pk>', views.RankingView.as_view(), name='ranking'),
 ]
